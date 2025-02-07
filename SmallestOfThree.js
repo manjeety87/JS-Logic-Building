48bcf4370eb904cbc242d194bc68fe63716ea8d1
“manjeety87” <manjeetyadav41000@gmail.com>
2025-02-06 22:31:25 -0500

    smallest of three done

---
+++ SmallestOfThree.js
@@ -0,0 +1,45 @@
+// write num1 function that finds smallest number among three input numbers
+// without using inbuilt functions
+
+const smallestOfThree = (num1, num2, num3) => {
+  if (num1 <= num2 && num1 <= num3) {
+    return num1;
+  } else if (num2 <= num1 && num2 <= num3) {
+    return num2;
+  } else if (num3 <= num1 && num3 <= num2) {
+    return num3;
+  }
+  return num1;
+};
+
+const smallestOfThree2 = (num1, num2, num3) => {
+  let smallestNumber = num1;
+  if (num2 < smallestNumber) {
+    smallestNumber = num2;
+  } else if (num3 < smallestNumber) {
+    smallestNumber = num3;
+  }
+  return smallestNumber;
+};
+
+console.log("Smallest Number from (2, 5, -1) is :", smallestOfThree(2, 5, -1));
+console.log(
+  "Smallest Number from (-2, 5, -10) is :",
+  smallestOfThree(-2, 5, -10)
+);
+console.log("Smallest Number from (7, 7, 7) is :", smallestOfThree(7, 7, 7));
+console.log("Smallest Number from (2, 7, 7) is :", smallestOfThree(2, 7, 7));
+console.log("Smallest Number from (7, 9, -7) is :", smallestOfThree2(7, 9, -7));
+
+console.log("");
+console.log("");
+console.log("");
+
+console.log("Smallest Number from (2, 5, -1) is :", smallestOfThree2(2, 5, -1));
+console.log(
+  "Smallest Number from (-2, 5, -10) is :",
+  smallestOfThree2(-2, 5, -10)
+);
+console.log("Smallest Number from (7, 7, 7) is :", smallestOfThree2(7, 7, 7));
+console.log("Smallest Number from (2, 7, 7) is :", smallestOfThree2(2, 7, 7));
+console.log("Smallest Number from (7, 9, -7) is :", smallestOfThree2(7, 9, -7));

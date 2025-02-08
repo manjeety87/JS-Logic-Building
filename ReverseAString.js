b0d136be8663c6a695e759d681c180425d442f4d
“manjeety87” <manjeetyadav41000@gmail.com>
2025-02-07 22:57:59 -0500

    revrese of string with one type of solution

---
+++ ReverseAString.js
@@ -0,0 +1,11 @@
+// write a fucntion that returns the reverse of as string
+
+const reverseString = (inputString) => {
+    for (i = inputString.length - 1; i >= 0; i--) {
+        console.log("::::::::",currentString[i])
+        
+    }
+    // const currentString = inputString.split("");
+};
+
+reverseString("Manjeet");

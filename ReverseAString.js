1c0b31fc226946a6a8110d7037596a6bca74aafb
“manjeety87” <manjeetyadav41000@gmail.com>
2025-02-07 23:04:04 -0500

    reverse of a string using slpit method

--- ReverseAString.js
+++ ReverseAString.js
@@ -5,8 +5,20 @@ const reverseString = (inputString) => {
   for (let i = inputString.length - 1; i >= 0; i--) {
     currentString = currentString + inputString[i];
   }
-  console.log("::::::::", currentString);
+  console.log("REVRESE USING LOOPS", currentString);
 };
 
 reverseString("Manjeet");
 reverseString("Manjeet is a begineer");
+
+const reverseString2 = (inputString) => {
+  let currentString = inputString.split("");
+  let result = ""
+  for (let i = currentString.length - 1; i >= 0; i--) {
+    result = result + inputString[i];
+  }
+  console.log("REVERSE USING SLPIT", result);
+};
+
+reverseString2("Manjeet");
+reverseString2("Manjeet is a begineer");

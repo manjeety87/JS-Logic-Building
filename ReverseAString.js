8187a4a7d35459ad08bdc9c015d8299a34fa183d
“manjeety87” <manjeetyadav41000@gmail.com>
2025-02-07 23:18:32 -0500

    added type check

--- ReverseAString.js
+++ ReverseAString.js
@@ -1,6 +1,10 @@
 // write a fucntion that returns the reverse of as string
 
 const reverseString = (inputString) => {
+  if (typeof inputString !== "string") {
+    console.log("Please enter a string");
+    return;
+  }
   let currentString = "";
   for (let i = inputString.length - 1; i >= 0; i--) {
     currentString = currentString + inputString[i];
@@ -12,8 +16,12 @@ reverseString("Manjeet");
 reverseString("Manjeet is a begineer");
 
 const reverseString2 = (inputString) => {
+  if (typeof inputString !== "string") {
+    console.log("Please enter a string");
+    return;
+  }
   let currentString = inputString.split("");
-  let result = ""
+  let result = "";
   for (let i = currentString.length - 1; i >= 0; i--) {
     result = result + inputString[i];
   }
@@ -21,4 +29,5 @@ const reverseString2 = (inputString) => {
 };
 
 reverseString2("Manjeet");
+reverseString2(12478);
 reverseString2("Manjeet is a begineer");

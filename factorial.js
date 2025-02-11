40a995024ad0012459c57c82964a97dd78b4cd4f
“manjeety87” <manjeetyadav41000@gmail.com>
2025-02-10 23:43:00 -0500

    factorial with diff type and a check

--- factorial.js
+++ factorial.js
@@ -9,3 +9,19 @@ const factorial = (num) => {
 factorial(3);
 factorial(10);
 factorial(5);
+
+const factorial2 = (num) => {
+  // tried with increment too with type check
+  if (typeof num === "number") {
+    let result = 1;
+    for (let i = 1; i <= num; i++) {
+      result = result * i;
+    }
+    console.log(`Factorial of ${num} is: `, result);
+  } else {
+    console.log("Enter a number to get a factorial!");
+  }
+};
+factorial2('3');
+factorial2(10);
+factorial2(5);

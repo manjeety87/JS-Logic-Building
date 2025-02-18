5ceab6326411b23df9b5f7a90ea63f58dae2d929
“manjeety87” <manjeetyadav41000@gmail.com>
2025-02-17 23:54:32 -0500

    sum of nums scenarios covered

--- SumOfNumbers.js
+++ SumOfNumbers.js
@@ -1,45 +1,57 @@
 // Write a funtion that prints sum of a number
 const sumOfNumber = (num) => {
-  let numString = num.toString();
-  let splittedString = numString.split("");
-  let sum = 0;
-  splittedString.map((n) => {
-    sum = sum + parseInt(n);
-  });
-  console.log(`SUM of ${num} is:`, sum);
+  if (num >= 0) {
+    let numString = num.toString();
+    let splittedString = numString.split("");
+    let sum = 0;
+    splittedString.map((n) => {
+      sum = sum + parseInt(n);
+    });
+    console.log(`SUM of ${num} is:`, sum);
+  } else {
+    console.log("Please enter a positive number!");
+  }
 };
 
 // Using forEach
 const sumOfNumber2 = (num) => {
-  let numString = num.toString();
-  let splittedString = numString.split("");
-  let sum = 0;
-  splittedString.forEach((n) => {
-    sum = sum + parseInt(n);
-  });
-  console.log(`SUM of ${num} is:`, sum);
+  if (num >= 0) {
+    let numString = num.toString();
+    let splittedString = numString.split("");
+    let sum = 0;
+    splittedString.forEach((n) => {
+      sum = sum + parseInt(n);
+    });
+    console.log(`SUM of ${num} is:`, sum);
+  } else {
+    console.log("Please enter a positive number!");
+  }
 };
 
 // Using remainder method
 const sumOfNumber3 = (num) => {
-  let number = num;
-  let sum = 0;
-  while (number > 0) {
-    let remainder = number % 10;
-    sum = sum + remainder;
-    number = Math.floor(number / 10);
+  if (num >= 0) {
+    let number = num;
+    let sum = 0;
+    while (number > 0) {
+      let remainder = number % 10;
+      sum = sum + remainder;
+      number = Math.floor(number / 10);
+    }
+    console.log(`SUM of ${num} is:`, sum);
+  } else {
+    console.log("Please enter a positive number!");
   }
-  console.log(`SUM of ${num} is:`, sum);
 };
 
-sumOfNumber(123);
+sumOfNumber(-123);
 sumOfNumber(124);
 sumOfNumber(321);
 
 sumOfNumber2(123);
 sumOfNumber2(124);
-sumOfNumber2(32);
+sumOfNumber2("-32");
 
 sumOfNumber3(123);
 sumOfNumber3(124);
-sumOfNumber3("32");
+sumOfNumber3(-111);

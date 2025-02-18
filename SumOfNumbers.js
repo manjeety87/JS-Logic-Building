47e2ae7d0a50d5334e1f51ccc5f406ca023304a9
“manjeety87” <manjeetyadav41000@gmail.com>
2025-02-17 23:36:45 -0500

    sum of nums using remainder method

--- SumOfNumbers.js
+++ SumOfNumbers.js
@@ -1,5 +1,5 @@
 // Write a funtion that prints sum of a number
-const sumOfNumber2 = (num) => {
+const sumOfNumber = (num) => {
   let numString = num.toString();
   let splittedString = numString.split("");
   let sum = 0;
@@ -9,6 +9,37 @@ const sumOfNumber2 = (num) => {
   console.log(`SUM of ${num} is:`, sum);
 };
 
+// Using forEach
+const sumOfNumber2 = (num) => {
+  let numString = num.toString();
+  let splittedString = numString.split("");
+  let sum = 0;
+  splittedString.forEach((n) => {
+    sum = sum + parseInt(n);
+  });
+  console.log(`SUM of ${num} is:`, sum);
+};
+
+// Using remainder method
+const sumOfNumber3 = (num) => {
+  let number = num;
+  let sum = 0;
+  while (number > 0) {
+    let remainder = number % 10;
+    sum = sum + remainder;
+    number = Math.floor(number / 10);
+  }
+  console.log(`SUM of ${num} is:`, sum);
+};
+
+sumOfNumber(123);
+sumOfNumber(124);
+sumOfNumber(321);
+
 sumOfNumber2(123);
-sumOfNumber2(321);
 sumOfNumber2(124);
+sumOfNumber2(32);
+
+sumOfNumber3(123);
+sumOfNumber3(124);
+sumOfNumber3("32");

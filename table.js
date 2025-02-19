2c3477ed7fb84f88f2bc68793f436d716e598ada
“manjeety87” <manjeetyadav41000@gmail.com>
2025-02-19 00:51:03 -0500

    table using while function

--- table.js
+++ table.js
@@ -1,4 +1,4 @@
-// Write a fucntion that generates and prints a multiplication table for a given number up to a specified number 
+// Write a fucntion that generates and prints a multiplication table for a given number up to a specified number
 const table = (num, range) => {
   if (num > 0 && range > 0) {
     for (let i = 1; i <= range; i++) {
@@ -9,11 +9,37 @@ const table = (num, range) => {
   }
 };
 
-table(5, -10);
+// table(5, -10);
+// console.log("");
+// table(5, 60);
+// console.log("");
+// table(-5, 3);
+// console.log("");
+// console.log("String");
+// table("5", "3");
+
+const table2 = (num, range) => {
+  let count = 1;
+  if ((num > 0 && range > 0) && typeof num === "number" && typeof range === "number") {
+    while (true) {
+      console.log(`${num} x ${count} = ${num * count}`);
+      count = count + 1;
+      if (count == range + 1) {
+        break;
+      }
+    }
+  } else {
+    console.log(`Please enter positive integer to print table!`);
+  }
+};
+
+table2(5, -10);
 console.log("");
-table(5, 60);
+table2(5, 60);
 console.log("");
-table(-5, 3);
+table2(5, 3);
 console.log("");
 console.log("String");
-table("5", "3");
+table2("5", "3");
+console.log("NUMBER");
+table2(5, 3);

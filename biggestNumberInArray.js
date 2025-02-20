46cc6df782fe0151cfbbd5029f32a44bb9370ad7
“manjeety87” <manjeetyadav41000@gmail.com>
2025-02-19 23:46:50 -0500

    Biggest Number In Array

---
+++ biggestNumberInArray.js
@@ -0,0 +1,13 @@
+// write a fucntion that finds and prints the maximum element in an array of numbers
+
+const biggestNumberInArray = (arr) => {
+  let max = arr[0];
+  for (let i = 0; i <= arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  console.log("Maxium number is ", max);
+};
+
+biggestNumberInArray([2,"10",8,7,9,1,"87"]);

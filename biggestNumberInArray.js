1a798e402de322e9d86b8da57d09ed8543b4f36b
“manjeety87” <manjeetyadav41000@gmail.com>
2025-02-20 00:31:38 -0500

    biggest Number In Array using spread operator

--- biggestNumberInArray.js
+++ biggestNumberInArray.js
@@ -1,13 +1,28 @@
 // write a fucntion that finds and prints the maximum element in an array of numbers
 
 const biggestNumberInArray = (arr) => {
-  let max = arr[0];
-  for (let i = 0; i <= arr.length; i++) {
-    if (arr[i] > max) {
-      max = arr[i];
+  if (arr.length > 0) {
+    let max = arr[0];
+    for (let i = 0; i <= arr.length; i++) {
+      if (arr[i] > max) {
+        max = arr[i];
+      }
     }
+    console.log("Maxium number is ", max);
+  } else {
+    console.log("Array is empty!");
   }
-  console.log("Maxium number is ", max);
 };
 
-biggestNumberInArray([2,"10",8,7,9,1,"87"]);
+biggestNumberInArray([2, "10", 8, 7, 9, 1, "87"]);
+
+const biggestNumberInArray2 = (arr) => {
+  if (arr.length > 0) {
+    let max = Math.max(...arr);
+    console.log("Maxium number is ", max);
+  } else {
+    console.log("Array is empty!");
+  }
+};
+
+biggestNumberInArray2([2, "10", 8, 7, 9, 1, "101", "87", 99]);

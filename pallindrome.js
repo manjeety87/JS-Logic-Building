f73ccb35b3f4a8c30e1678e649b38cdd9b84cef1
“manjeety87” <manjeetyadav41000@gmail.com>
2025-02-22 20:05:35 -0500

    Pallindrome using split method

--- pallindrome.js
+++ pallindrome.js
@@ -17,5 +17,32 @@ const checkPallidrome = (str) => {
 checkPallidrome("lolo");
 checkPallidrome("hurruh");
 checkPallidrome("madam");
+checkPallidrome("Manjeet");
 checkPallidrome(878);
 checkPallidrome("LEVEL");
+
+console.log("");
+console.log("Pallinndrome using SLPIT Method");
+console.log("");
+
+const checkPallidrome2 = (str) => {
+  if (typeof str === "string") {
+    let strArray = str.split("");
+    let tempStr = "";
+    for (let i = strArray.length - 1; i >= 0; i--) {
+      tempStr = tempStr + strArray[i];
+    }
+    console.log(
+      tempStr === str ? `${str} is Pallindrome` : `${str} is Not Pallindrome`
+    );
+  } else {
+    console.log("Please pass string variable to check pallindrome");
+  }
+};
+
+checkPallidrome2("lolo");
+checkPallidrome2("hurruh");
+checkPallidrome2("madam");
+checkPallidrome2("Manjeet");
+checkPallidrome2(878);
+checkPallidrome2("LEVEL");

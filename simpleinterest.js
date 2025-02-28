c1d0c08223c730fdec56177b5ba443024ce718a6
“manjeety87” <manjeetyadav41000@gmail.com>
2025-02-28 14:00:49 -0500

    simple interest program handled scenarios

--- simpleinterest.js
+++ simpleinterest.js
@@ -1,6 +1,12 @@
 // write a function that calculates and prints simple interest on a loan amount.
 
 const calculateSimpleInterest = (interestRate, principleAmount, duration) => {
+  if (
+    typeof interestRate !== "number" ||
+    typeof principleAmount !== "number" ||
+    typeof duration !== "number"
+  )
+    return console.log("Please pass numbers to calculate simple interest.");
   let simpleInterest = (interestRate * principleAmount * duration) / 100;
   simpleInterest = simpleInterest.toFixed(2);
   console.log(simpleInterest);
@@ -9,3 +15,4 @@ const calculateSimpleInterest = (interestRate, principleAmount, duration) => {
 calculateSimpleInterest(1000, 5, 1);
 calculateSimpleInterest(20000, 10, 5);
 calculateSimpleInterest(20000.88, 10.775, 5);
+calculateSimpleInterest(20000.88, 10.775, "5");
